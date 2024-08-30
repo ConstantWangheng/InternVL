d4629a7c97a01627f8bf1780e833e2b300519b2c
ConstantWangheng <2282821376@qq.com>
2024-08-30 10:54:18 +0800

    Update modeling_intern_vit.py

--- internvl_chat/internvl/model/internvl_chat/modeling_intern_vit.py
+++ internvl_chat/internvl/model/internvl_chat/modeling_intern_vit.py
@@ -71,6 +71,8 @@ class InternVisionEmbeddings(nn.Module):
         self.image_size = config.image_size
         self.patch_size = config.patch_size
 
+        # Input Sequence: [CLS, Patch1, Patch2, ..., PatchN]
+        # 表示全局的信息
         self.class_embedding = nn.Parameter(
             torch.randn(1, 1, self.embed_dim),
         )
@@ -79,12 +81,18 @@ class InternVisionEmbeddings(nn.Module):
             in_channels=3, out_channels=self.embed_dim, kernel_size=self.patch_size, stride=self.patch_size
         )
 
+        # 448 // 14 = 32
+        # 32 * 32 = 1024 个patch,每张448*448的图表示成1024个token
         self.num_patches = (self.image_size // self.patch_size) ** 2
         self.num_positions = self.num_patches + 1
 
         self.position_embedding = nn.Parameter(torch.randn(1, self.num_positions, self.embed_dim))
 
     def _get_pos_embed(self, pos_embed, H, W):
+        """
+        二维位置编码（2D Position Embedding），其作用是对不同分辨率的图像进行位置嵌入的插值，使得位置编码能够适应不
+        同尺寸的图像。通过插值操作使得预训练的图像位置嵌入能够在不同的图像尺寸下仍然适用。
+        """
         target_dtype = pos_embed.dtype
         pos_embed = pos_embed.float().reshape(
             1, self.image_size // self.patch_size, self.image_size // self.patch_size, -1).permute(0, 3, 1, 2)
@@ -94,7 +102,9 @@ class InternVisionEmbeddings(nn.Module):
 
     def forward(self, pixel_values: torch.FloatTensor) -> torch.Tensor:
         target_dtype = self.patch_embedding.weight.dtype
+      
         patch_embeds = self.patch_embedding(pixel_values)  # shape = [*, channel, width, height]
+        # 对输入的patch进行卷积变换
         batch_size, _, height, width = patch_embeds.shape
         patch_embeds = patch_embeds.flatten(2).transpose(1, 2)
         class_embeds = self.class_embedding.expand(batch_size, 1, -1).to(target_dtype)
